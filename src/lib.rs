abb2be1726c47e985607b40f5e5de2a028f3ab48
David Tolnay <dtolnay@gmail.com>
2022-07-06 22:36:08 -0700

    Remove unnamed struct and union types from public API

--- src/lib.rs
+++ src/lib.rs
@@ -221,12 +221,78 @@ mod scanner;
 mod writer;
 mod yaml;
 
-pub use crate::api::*;
-pub use crate::dumper::*;
-pub use crate::emitter::*;
-pub use crate::loader::*;
-pub use crate::parser::*;
-pub use crate::reader::*;
-pub use crate::scanner::*;
-pub use crate::writer::*;
-pub use crate::yaml::*;
+pub use crate::api::{
+    yaml_alias_event_initialize, yaml_document_add_mapping, yaml_document_add_scalar,
+    yaml_document_add_sequence, yaml_document_append_mapping_pair,
+    yaml_document_append_sequence_item, yaml_document_delete, yaml_document_end_event_initialize,
+    yaml_document_get_node, yaml_document_get_root_node, yaml_document_initialize,
+    yaml_document_start_event_initialize, yaml_emitter_delete, yaml_emitter_initialize,
+    yaml_emitter_set_break, yaml_emitter_set_canonical, yaml_emitter_set_encoding,
+    yaml_emitter_set_indent, yaml_emitter_set_output, yaml_emitter_set_output_string,
+    yaml_emitter_set_unicode, yaml_emitter_set_width, yaml_event_delete, yaml_free,
+    yaml_get_version, yaml_get_version_string, yaml_malloc, yaml_mapping_end_event_initialize,
+    yaml_mapping_start_event_initialize, yaml_parser_delete, yaml_parser_initialize,
+    yaml_parser_set_encoding, yaml_parser_set_input, yaml_parser_set_input_string,
+    yaml_queue_extend, yaml_realloc, yaml_scalar_event_initialize,
+    yaml_sequence_end_event_initialize, yaml_sequence_start_event_initialize, yaml_stack_extend,
+    yaml_strdup, yaml_stream_end_event_initialize, yaml_stream_start_event_initialize,
+    yaml_string_extend, yaml_string_join, yaml_token_delete,
+};
+pub use crate::dumper::{yaml_emitter_close, yaml_emitter_dump, yaml_emitter_open};
+pub use crate::emitter::yaml_emitter_emit;
+pub use crate::loader::{loader_ctx, yaml_parser_load};
+pub use crate::parser::yaml_parser_parse;
+pub use crate::reader::yaml_parser_update_buffer;
+pub use crate::scanner::{yaml_parser_fetch_more_tokens, yaml_parser_scan};
+pub use crate::writer::yaml_emitter_flush;
+pub use crate::yaml::{
+    ptrdiff_t, size_t, yaml_alias_data_s, yaml_alias_data_t, yaml_anchors_s, yaml_anchors_t,
+    yaml_break_e, yaml_break_t, yaml_char_t, yaml_document_s, yaml_document_t, yaml_emitter_s,
+    yaml_emitter_state_e, yaml_emitter_state_t, yaml_emitter_t, yaml_encoding_e, yaml_encoding_t,
+    yaml_error_type_e, yaml_error_type_t, yaml_event_s, yaml_event_t, yaml_event_type_e,
+    yaml_event_type_t, yaml_mapping_style_e, yaml_mapping_style_t, yaml_mark_s, yaml_mark_t,
+    yaml_node_item_t, yaml_node_pair_s, yaml_node_pair_t, yaml_node_s, yaml_node_t,
+    yaml_node_type_e, yaml_node_type_t, yaml_parser_s, yaml_parser_state_e, yaml_parser_state_t,
+    yaml_parser_t, yaml_read_handler_t, yaml_scalar_style_e, yaml_scalar_style_t,
+    yaml_sequence_style_e, yaml_sequence_style_t, yaml_simple_key_s, yaml_simple_key_t,
+    yaml_string_t, yaml_tag_directive_s, yaml_tag_directive_t, yaml_token_s, yaml_token_t,
+    yaml_token_type_e, yaml_token_type_t, yaml_version_directive_s, yaml_version_directive_t,
+    yaml_write_handler_t, YAML_ALIAS_EVENT, YAML_ALIAS_TOKEN, YAML_ANCHOR_TOKEN, YAML_ANY_BREAK,
+    YAML_ANY_ENCODING, YAML_ANY_MAPPING_STYLE, YAML_ANY_SCALAR_STYLE, YAML_ANY_SEQUENCE_STYLE,
+    YAML_BLOCK_END_TOKEN, YAML_BLOCK_ENTRY_TOKEN, YAML_BLOCK_MAPPING_START_TOKEN,
+    YAML_BLOCK_MAPPING_STYLE, YAML_BLOCK_SEQUENCE_START_TOKEN, YAML_BLOCK_SEQUENCE_STYLE,
+    YAML_COMPOSER_ERROR, YAML_CRLN_BREAK, YAML_CR_BREAK, YAML_DOCUMENT_END_EVENT,
+    YAML_DOCUMENT_END_TOKEN, YAML_DOCUMENT_START_EVENT, YAML_DOCUMENT_START_TOKEN,
+    YAML_DOUBLE_QUOTED_SCALAR_STYLE, YAML_EMITTER_ERROR, YAML_EMIT_BLOCK_MAPPING_FIRST_KEY_STATE,
+    YAML_EMIT_BLOCK_MAPPING_KEY_STATE, YAML_EMIT_BLOCK_MAPPING_SIMPLE_VALUE_STATE,
+    YAML_EMIT_BLOCK_MAPPING_VALUE_STATE, YAML_EMIT_BLOCK_SEQUENCE_FIRST_ITEM_STATE,
+    YAML_EMIT_BLOCK_SEQUENCE_ITEM_STATE, YAML_EMIT_DOCUMENT_CONTENT_STATE,
+    YAML_EMIT_DOCUMENT_END_STATE, YAML_EMIT_DOCUMENT_START_STATE, YAML_EMIT_END_STATE,
+    YAML_EMIT_FIRST_DOCUMENT_START_STATE, YAML_EMIT_FLOW_MAPPING_FIRST_KEY_STATE,
+    YAML_EMIT_FLOW_MAPPING_KEY_STATE, YAML_EMIT_FLOW_MAPPING_SIMPLE_VALUE_STATE,
+    YAML_EMIT_FLOW_MAPPING_VALUE_STATE, YAML_EMIT_FLOW_SEQUENCE_FIRST_ITEM_STATE,
+    YAML_EMIT_FLOW_SEQUENCE_ITEM_STATE, YAML_EMIT_STREAM_START_STATE, YAML_FLOW_ENTRY_TOKEN,
+    YAML_FLOW_MAPPING_END_TOKEN, YAML_FLOW_MAPPING_START_TOKEN, YAML_FLOW_MAPPING_STYLE,
+    YAML_FLOW_SEQUENCE_END_TOKEN, YAML_FLOW_SEQUENCE_START_TOKEN, YAML_FLOW_SEQUENCE_STYLE,
+    YAML_FOLDED_SCALAR_STYLE, YAML_KEY_TOKEN, YAML_LITERAL_SCALAR_STYLE, YAML_LN_BREAK,
+    YAML_MAPPING_END_EVENT, YAML_MAPPING_NODE, YAML_MAPPING_START_EVENT, YAML_MEMORY_ERROR,
+    YAML_NO_ERROR, YAML_NO_EVENT, YAML_NO_NODE, YAML_NO_TOKEN, YAML_PARSER_ERROR,
+    YAML_PARSE_BLOCK_MAPPING_FIRST_KEY_STATE, YAML_PARSE_BLOCK_MAPPING_KEY_STATE,
+    YAML_PARSE_BLOCK_MAPPING_VALUE_STATE, YAML_PARSE_BLOCK_NODE_OR_INDENTLESS_SEQUENCE_STATE,
+    YAML_PARSE_BLOCK_NODE_STATE, YAML_PARSE_BLOCK_SEQUENCE_ENTRY_STATE,
+    YAML_PARSE_BLOCK_SEQUENCE_FIRST_ENTRY_STATE, YAML_PARSE_DOCUMENT_CONTENT_STATE,
+    YAML_PARSE_DOCUMENT_END_STATE, YAML_PARSE_DOCUMENT_START_STATE, YAML_PARSE_END_STATE,
+    YAML_PARSE_FLOW_MAPPING_EMPTY_VALUE_STATE, YAML_PARSE_FLOW_MAPPING_FIRST_KEY_STATE,
+    YAML_PARSE_FLOW_MAPPING_KEY_STATE, YAML_PARSE_FLOW_MAPPING_VALUE_STATE,
+    YAML_PARSE_FLOW_NODE_STATE, YAML_PARSE_FLOW_SEQUENCE_ENTRY_MAPPING_END_STATE,
+    YAML_PARSE_FLOW_SEQUENCE_ENTRY_MAPPING_KEY_STATE,
+    YAML_PARSE_FLOW_SEQUENCE_ENTRY_MAPPING_VALUE_STATE, YAML_PARSE_FLOW_SEQUENCE_ENTRY_STATE,
+    YAML_PARSE_FLOW_SEQUENCE_FIRST_ENTRY_STATE, YAML_PARSE_IMPLICIT_DOCUMENT_START_STATE,
+    YAML_PARSE_INDENTLESS_SEQUENCE_ENTRY_STATE, YAML_PARSE_STREAM_START_STATE,
+    YAML_PLAIN_SCALAR_STYLE, YAML_READER_ERROR, YAML_SCALAR_EVENT, YAML_SCALAR_NODE,
+    YAML_SCALAR_TOKEN, YAML_SCANNER_ERROR, YAML_SEQUENCE_END_EVENT, YAML_SEQUENCE_NODE,
+    YAML_SEQUENCE_START_EVENT, YAML_SINGLE_QUOTED_SCALAR_STYLE, YAML_STREAM_END_EVENT,
+    YAML_STREAM_END_TOKEN, YAML_STREAM_START_EVENT, YAML_STREAM_START_TOKEN,
+    YAML_TAG_DIRECTIVE_TOKEN, YAML_TAG_TOKEN, YAML_UTF16BE_ENCODING, YAML_UTF16LE_ENCODING,
+    YAML_UTF8_ENCODING, YAML_VALUE_TOKEN, YAML_VERSION_DIRECTIVE_TOKEN, YAML_WRITER_ERROR,
+};
