52958a5d8767c6d9e4e45434bdf293e531ec7f8f
David Tolnay <dtolnay@gmail.com>
2022-07-08 12:12:46 -0700

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! [![github]](https://github.com/dtolnay/unsafe-libyaml)&ensp;[![crates-io]](https://crates.io/crates/unsafe-libyaml)&ensp;[![docs-rs]](https://docs.rs/unsafe-libyaml)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+
 #![no_std]
 #![doc(html_root_url = "https://docs.rs/unsafe-libyaml/0.0.0")]
 #![allow(non_camel_case_types)]
