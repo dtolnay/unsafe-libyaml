924ba2d6346ba16c5394f0abe44acdd5d3eb3ec0
David Tolnay <dtolnay@gmail.com>
2022-07-22 22:31:29 -0700

    Simplify MOVE macro

--- src/macros.rs
+++ src/macros.rs
@@ -333,19 +333,7 @@ macro_rules! WIDTH {
 
 macro_rules! MOVE {
     ($string:expr) => {
-        $string.pointer = $string.pointer.wrapping_offset(
-            (if *$string.pointer as libc::c_int & 0x80_i32 == 0_i32 {
-                1_i32
-            } else if *$string.pointer as libc::c_int & 0xe0_i32 == 0xc0_i32 {
-                2_i32
-            } else if *$string.pointer as libc::c_int & 0xf0_i32 == 0xe0_i32 {
-                3_i32
-            } else if *$string.pointer as libc::c_int & 0xf8_i32 == 0xf0_i32 {
-                4_i32
-            } else {
-                0_i32
-            }) as isize,
-        )
+        $string.pointer = $string.pointer.wrapping_offset(WIDTH!($string) as isize)
     };
 }
 
